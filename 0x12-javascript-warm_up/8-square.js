261af6dfe204029a19c0f92d175cda272e3c4e72
Motaz <motazmoneer71@gmail.com>
2024-06-19 03:56:36 +0400

    HW

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const size = process.argv[2];
+if (size === undefined || isNaN(parseInt(size))){
+  console.log('Missing size');
+} else{
+  for (let i = 0; i < parseInt(size); i++){
+    let row = '';
+    for (let j = 0; j < parseInt(size); j++) {
+      row +='X';  
+    }
+    console.log(row);
+  }
+}
